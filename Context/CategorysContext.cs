f80a6f292a2f24f0ef980da9165e33781d0ecdc9
Verka3585 <verochka.shishigina@yandex.ru>
2024-09-04 13:35:39 +0500

    Разработанные модели данных. lekontsev

---
+++ Context/CategorysContext.cs
@@ -0,0 +1,31 @@
+﻿using ShopContent_lekontsev.Classes;
+using ShopContent_lekontsev.Modell;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Text;
+
+
+namespace ShopContent_lekontsev.Context
+{
+    public class CategorysContext : Categorys
+    {
+        public static ObservableCollection<CategorysContext> AllCategorys()
+        {
+            ObservableCollection<CategorysContext> allCategorys = new ObservableCollection<CategorysContext>();
+            SqlConnection connection;
+            SqlDataReader dataCategorys = Connection.Query("SELECT * FROM [dbo].[Categorys]", out connection);
+            while (dataCategorys.Read())
+            {
+                allCategorys.Add(new CategorysContext()
+                {
+                    Id = dataCategorys.GetInt32(0),
+                    Name = dataCategorys.GetString(1)
+                });
+            }
+            Connection.CloseConnection(connection);
+            return allCategorys;
+        }
+    }
+}
